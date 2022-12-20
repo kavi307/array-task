d8f8c44c51e3f5c81f93174875bbbd1b3dbc03de
kavi307 <ajkavitha7@gmail.com>
2022-12-20 10:48:55 +0530

    array task

---
+++ main.js
@@ -0,0 +1,16 @@
+var foodsdata=[];
+var lenght=prompt(" lenght");
+for(i=0;i<lenght;i++)
+{
+     foodsdata[i]=prompt("your Foods");
+
+}
+document.write(foodsdata);
+document.write("<br>");
+        var change=prompt("change Index");
+        for(i=change;i<=change;i++)
+        {
+            foodsdata[i]=prompt("change value");
+        }   
+    
+document.write(foodsdata);
